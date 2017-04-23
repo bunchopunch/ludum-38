f2e912f887751eb75b39202c0b01aac52608b794
Sara <seking13@gmail.com>
2017-04-22 23:05:56 -0600

    workingOnMovement

--- public/js/play.js
+++ public/js/play.js
@@ -2,11 +2,48 @@ var playState = {
 
     create: function () {
 
-        var menuLabel = game.add.text(80, 150, 'Playing', {font: '30px Courier', fill: '#fff'});
-        game.add.image(95, 150, 'player');
+      game.add.plugin(Phaser.Plugin.Debug);
+      game.add.plugin(Phaser.Plugin.Inspector);
 
-        game.add.plugin(Phaser.Plugin.Debug);
-        game.add.plugin(Phaser.Plugin.Inspector);
+        var menuLabel = game.add.text(80, 150, 'Playing', {font: '30px Courier', fill: '#fff'});
+
+        game.stage.backgroundColor = '#124184';
+
+        //  Enable Box2d physics
+        game.physics.startSystem(Phaser.Physics.BOX2D);
+
+      var sprite = game.add.image(95, 150, 'player');
+
+        sprite.body.fixedRotation = true;
+
+        game.add.text(5, 5, 'Use arrow keys to move.', { fill: '#ffffff', font: '14pt Arial' });
+
+        var cursors = game.input.keyboard.createCursorKeys();
+    },
+
+
+update: function (){
+
+    sprite.body.setZeroVelocity();
+
+    if (cursors.left.isDown)
+    {
+        sprite.body.moveLeft(400);
     }
+    else if (cursors.right.isDown)
+    {
+        sprite.body.moveRight(400);
+    }
+
+    if (cursors.up.isDown)
+    {
+        sprite.body.moveUp(400);
+    }
+    else if (cursors.down.isDown)
+    {
+        sprite.body.moveDown(400);
+    }
+
+}
 
 };
