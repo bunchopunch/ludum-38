9319fe23d44ab17b6263162e72905fab2eac7086
bunchopunch <bunchopunch@mustacheforpresident.com>
2017-04-23 01:46:17 -0600

    Add and enhance graphics

--- public/js/load.js
+++ public/js/load.js
@@ -13,8 +13,10 @@ var loadState = {
         //Load your images, spritesheets, bitmaps...
         game.load.image('boiler-logo', 'assets/img/boilerplate-logo.png');
         game.load.image('player', 'assets/img/ship.png');
-        game.load.image('monster-left', 'assets/img/monster-left.png');
-        game.load.image('monster-right', 'assets/img/monster-right.png');
+        game.load.image('monster-left', 'assets/img/monster-pink.png');
+        game.load.image('monster-right', 'assets/img/monster-blue.png');
+        game.load.image('mob-blue-64', 'assets/img/mob-blue-64.png');
+        game.load.image('mob-pink-64', 'assets/img/mob-pink-64.png');
 
         //Load your sounds, efx, music...
         //Example: game.load.audio('rockas', 'assets/snd/rockas.wav');
--- public/js/play.js
+++ public/js/play.js
@@ -1,15 +1,14 @@
-
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+var player;
 
 var playState = {
 
-
     create: function () {
-
-
       var monsterLeft = game.add.tileSprite(0, 0, 128, 600, 'monster-left');
       var monsterRight = game.add.tileSprite(513, 0, 128, 600, 'monster-right');
-      var player = game.add.image(95, 150, 'player');
+      var monsterPink64 = game.add.image(150, 250, 'mob-pink-64');
+      var monsterBlue64 = game.add.image(435, 250, 'mob-blue-64');
+      player = game.add.image(325, 280, 'player');
+
       player.anchor.setTo(0.5, 0.5);
       player.scale.setTo(1, 1);
       player.animations.add('run');
@@ -18,21 +17,15 @@ var playState = {
 
     update: function (){
 
-            if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
-      {
+      if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
           player.x -= 4;
-      }
-      else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
-      {
+      } else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
           player.x += 4;
       }
 
-      if (game.input.keyboard.isDown(Phaser.Keyboard.UP))
-      {
+      if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
           player.y -= 4;
-      }
-      else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN))
-      {
+      } else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
           player.y += 4;
       }
     },
