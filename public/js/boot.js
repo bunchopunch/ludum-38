c76b85a5f14d036433ceede9de69cf041a64d2bc
bunchopunch <bunchopunch@mustacheforpresident.com>
2017-04-23 13:01:59 -0600

    Change physics engine to P2

--- public/js/boot.js
+++ public/js/boot.js
@@ -3,7 +3,7 @@ var bootState = {
     create: function () {
 
         //Initial GameSystem (Arcade, P2, Ninja)
-        game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.physics.startSystem(Phaser.Physics.P2JS);
 
         //Initial Load State
         game.state.start('load');
