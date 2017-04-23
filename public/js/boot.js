e4726f7b812461d57d347c9205345e8a4edc4208
bunchopunch <bunchopunch@mustacheforpresident.com>
2017-04-23 14:58:24 -0600

    Change to sprite for physics

--- public/js/boot.js
+++ public/js/boot.js
@@ -4,6 +4,7 @@ var bootState = {
 
         //Initial GameSystem (Arcade, P2, Ninja)
         game.physics.startSystem(Phaser.Physics.P2JS);
+        game.physics.p2.restitution = 0.9;
 
         //Initial Load State
         game.state.start('load');
