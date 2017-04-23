6602f8c8ef65b1fee717f6796a29f881c5e8a599
bunchopunch <bunchopunch@mustacheforpresident.com>
2017-04-22 20:46:45 -0600

    Add placeholder ship image

--- js/menu.js
+++ js/menu.js
@@ -2,9 +2,12 @@ var menuState = {
 
     create: function () {
 
-        game.add.plugin(Phaser.Plugin.Debug);
+        var MenuLabel = game.add.text(80, 150, 'Menu', {font: '30px Courier', fill: '#fff'});
         game.add.image(95, 150, 'boiler-logo');
+
+        game.add.plugin(Phaser.Plugin.Debug);
         game.add.plugin(Phaser.Plugin.Inspector);
+
 
     }
 
